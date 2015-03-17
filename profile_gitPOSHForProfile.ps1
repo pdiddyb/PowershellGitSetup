b0d3b07a38a371f211fd9481debbf606a1fbeb4f
Paul Brown <pablo@pablosweb.com>
2015-03-17 12:02:16 -0500

    nothing

--- profile_gitPOSHForProfile.ps1
+++ profile_gitPOSHForProfile.ps1
@@ -2,3 +2,4 @@ Remove-Variable -Force HOME
 Set-Variable HOME (Get-Item $profile).Directory.Parent.Parent.FullName
 . (Resolve-Path "$env:LOCALAPPDATA\GitHub\shell.ps1")
 . $env:github_posh_git\profile.example.ps1
+
